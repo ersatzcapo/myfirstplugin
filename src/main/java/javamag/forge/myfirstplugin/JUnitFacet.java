37e061f7b1cee10eccfd6aaf0dbfd28be60cbd78
Christian Brandenstein <cbr@adorsys.de>
2013-02-23 19:18:23 +0100

    Initial setup und JUnitFacet

---
+++ src/main/java/javamag/forge/myfirstplugin/JUnitFacet.java
@@ -0,0 +1,19 @@
+package javamag.forge.myfirstplugin;
+
+import org.jboss.forge.project.facets.BaseFacet;
+
+public class JUnitFacet extends BaseFacet {
+
+    @Override
+    public boolean install() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isInstalled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
